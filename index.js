f0d8fee071b8b4c1448a5b89753d22190d382a20
krushna bankar <n.bankar1977@gmail.com>
2026-04-01 23:58:41 +0530

    adding routes for basic CRUD operations

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const path = require('path')
 const app = express();
 const listing = require('C:/Users/Krushna/OneDrive/Desktop/Project1/First-FullStack-Project/collections/listing');
+const methodOverride = require("method-override")
+app.use(methodOverride("_method"))
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 main().then(() => console.log("DB Connected"));
@@ -30,11 +32,34 @@ app.get("/listing",async (req,res)=>{
     
 })
 
+// new listing add route
+app.get('/listing/new',(req,res)=>{
+    res.render('new.ejs');
+})
+
+app.post("/listing",async (req,res)=>{
+    const newListing = new listing(req.body.new);
+    await newListing.save().then((res)=>{
+        console.log('Added');
+    }).catch((err)=>{
+        console.log(err);
+    })
+    res.redirect("/listing")
+})
+
 // show route give info about perticular listing
 app.get("/listing/:id",async (req,res)=>{
     let {id} = req.params;
     const list = await listing.findById(id);
     console.log(list);
     res.render('show.ejs',{list}); 
+    
+})
 
+// edit and Update route
+
+app.get("/listing/:id/new",(req,res)=>{
+    let {id} = req.params;
+    const list = listing.findById(id);
+    res.render('edit.ejs',{list})
 })
