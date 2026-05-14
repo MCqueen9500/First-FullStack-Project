3fb3d19c79e4d49570d143408799f4f979382873
krushna bankar <n.bankar1977@gmail.com>
2026-05-14 18:59:12 +0530

    fix env loading on render

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const mongoose = require("mongoose");
 const path = require('path')
 const app = express();
