1337dc17df906f83d76db494aec0038c495a1a18
krushna bankar <n.bankar1977@gmail.com>
2026-05-15 01:05:07 +0530

    fix styling and env bugs

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ route.route('/login')
 .get(asyncwrap(userController.loginGet))
 .post(
     orgUrl, 
-    passport.authenticate('local',{failureRedirect:'/login',failureFlash:true}),//we can validateuser manually also but here we are using passport.authenticate() method which is inbuilt in passport-local-mongoose package and it will validate the user for us and if there is any error then it will flash the error message and redirect to login page
+    passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),//we can validateuser manually also but here we are using passport.authenticate() method which is inbuilt in passport-local-mongoose package and it will validate the user for us and if there is any error then it will flash the error message and redirect to login page
     asyncwrap(userController.loginPost))
 
 route.get('/logout',userController.logout);
